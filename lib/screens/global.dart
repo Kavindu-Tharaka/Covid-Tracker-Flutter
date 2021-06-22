5943cda03f4910a10d68a96f57a9f17258ddbacf
Tharaka W.C.M.K <it18001112@my.sliit.lk>
2021-06-23 00:08:42 +0530

    Add tab screens

---
+++ lib/screens/global.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Global extends StatefulWidget {
+  const Global({ Key? key }) : super(key: key);
+
+  @override
+  _GlobalState createState() => _GlobalState();
+}
+
+class _GlobalState extends State<Global> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Country'),
+    );
+  }
+}
